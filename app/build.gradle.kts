99c182982e380fdb1ea28be3f45352971165cfdc
Ina Burström <inaburstrom@hotmail.com>
2023-11-27 12:45:04 +0100

    Added CreateCardsActivity and created the initial layout.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.snapstudies"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.snapstudies"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
