88d3febade0bfe186937322e4d70c9683aa351be
Ina Burström <inaburstrom@hotmail.com>
2023-11-28 14:34:11 +0100

    Added StatisticsActivity and intents for the buttons in MainActivity

--- app/src/main/java/com/example/snapstudies/MainActivity.kt
+++ app/src/main/java/com/example/snapstudies/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.snapstudies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonAddWords: Button = findViewById(R.id.buttonAddWords)
+        val buttonStatistics: Button = findViewById(R.id.buttonStatistics)
+        val buttonStartPractice: Button = findViewById(R.id.buttonStartPractice)
+
+        buttonAddWords.setOnClickListener {
+            val intent = Intent(this, CreateCardsActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonStatistics.setOnClickListener {
+            val intent = Intent(this, StaticsActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonStartPractice.setOnClickListener {
+            val intent = Intent(this, PracticeActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
     }
 }
