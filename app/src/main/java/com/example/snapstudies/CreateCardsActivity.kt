01bfcb0eb76337905c7f7549667de3cc99b0f056
Ina Burström <inaburstrom@hotmail.com>
2023-12-01 14:03:40 +0100

    Added requestFocus so the marker moves back to swedish text input again after adding a word.

--- app/src/main/java/com/example/snapstudies/CreateCardsActivity.kt
+++ app/src/main/java/com/example/snapstudies/CreateCardsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class CreateCardsActivity : AppCompatActivity() {
@@ -16,12 +17,16 @@ class CreateCardsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_cards)
 
-        val sharedPreferencesManager = SharedPreferenceManager(this)
-        glossaryList = HashMap()
         val buttonAddWord = findViewById<Button>(R.id.buttonAddWord)
         val buttonDone = findViewById<Button>(R.id.buttonDone)
         val swedishKey = findViewById<EditText>(R.id.editKeySwedishWord)
         val englishValue = findViewById<EditText>(R.id.editValueEnglishWord)
+        val textViewWordNo = findViewById<TextView>(R.id.textViewWordNo)
+        val sharedPreferencesManager = SharedPreferenceManager(this)
+        var wordCounter = 1
+
+        glossaryList = HashMap()
+        updateWordNumber(textViewWordNo, wordCounter)
 
         buttonAddWord.setOnClickListener {
             val key = swedishKey.text.toString()
@@ -29,7 +34,10 @@ class CreateCardsActivity : AppCompatActivity() {
             if (key.isNotBlank() && value.isNotBlank()) {
                 glossaryList.put(key, value)
                 swedishKey.setText("")
+                swedishKey.requestFocus()
                 englishValue.setText("")
+                wordCounter ++
+                updateWordNumber(textViewWordNo, wordCounter)
                 Log.d("!!!", glossaryList.toString())
             } else {
                 Toast.makeText(this, "Fyll i orden korrekt.", Toast.LENGTH_SHORT).show()
@@ -38,14 +46,18 @@ class CreateCardsActivity : AppCompatActivity() {
 
         buttonDone.setOnClickListener {
             if (glossaryList.size > 7) {
-                Log.d("!!!", "Done button clicked - before saving data")
                 sharedPreferencesManager.saveData("user_glossary_list", glossaryList)
-                Log.d("!!!", "Done button clicked - after saving data")
+                Log.d("!!!", glossaryList.toString())
                 val intent = Intent(this, PracticeActivity::class.java)
                 startActivity(intent)
             } else {
                 Toast.makeText(this, "Lägg till fler ord.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    fun updateWordNumber(textViewWordNo: TextView, wordCounter: Int){
+        val wordNumber = getString(R.string.textview_word_no, wordCounter)
+        textViewWordNo.text = wordNumber
     }
 }
