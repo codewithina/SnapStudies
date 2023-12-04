ef51726f3b5c27190ad8554723bfbde6641201e3
Ina Burström <inaburstrom@hotmail.com>
2023-12-04 12:06:22 +0100

    Added startpage with logo that fades out to mainactivity.

--- app/src/main/java/com/example/snapstudies/MainActivity.kt
+++ app/src/main/java/com/example/snapstudies/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.snapstudies
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBg = findViewById<View>(R.id.startFadeOut)
+        val startLogo = findViewById<ImageView>(R.id.logoFadeOut)
+
+        startFadeOut(startBg, startLogo)
 
         val buttonAddWords: Button = findViewById(R.id.buttonAddWords)
         val buttonStatistics: Button = findViewById(R.id.buttonStatistics)
@@ -36,5 +44,17 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Skapa gloslista först.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    fun startFadeOut (startBg : View, startLogo : ImageView){
+
+        val fadeOutBg = ObjectAnimator.ofFloat(startBg, "alpha", 1.0f, 0.0f)
+        fadeOutBg.duration = 3000
+        val fadeOutLogo = ObjectAnimator.ofFloat(startLogo, "alpha", 1.0f, 0.0f)
+        fadeOutLogo.duration = 3000
+
+        fadeOutBg.start()
+        fadeOutLogo.start()
+
     }
 }
